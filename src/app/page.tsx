be905abb2dd81cd7082da42993f509eb7938ef8f
Elton <eltonwhybrow@icloud.com>
2024-03-03 07:55:29 +0000

    feat: link up prismic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="font-bold font-display text-4xl">Hello world!</h1>
+      <h1 className="font-bold font-display text-4xl">Hello world!!</h1>
     </main>
   );
 }
